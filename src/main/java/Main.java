6d13eb35463b9b85b7717dafec11ed99273e71a7
ritcat14 <ritcat14@gmail.com>
2017-10-25 16:58:41 +0000

    Added main class and basic game loop

---
+++ src/main/java/Main.java
@@ -0,0 +1,46 @@
+
+public class Main implements Runnable {
+    
+    private Thread t;
+    private boolean running = false;
+    
+    public Main() {
+        
+    }
+    
+    public synchronized void start() {
+        running = true;
+        t = new Thread(this, "Main");
+        t.start();
+    }
+    
+    public synchronized void stop() {
+        try {
+            t.join();
+        } catch (Exception e) {}
+        running = false;
+    }
+    
+    public void update() { // Update game logic
+        
+    }
+    
+    public void render() { // Draw the game
+        
+    }
+    
+    @Override
+    public void run() {
+        while(running) { // Game loop
+            update();
+            render();
+        }
+    }
+    
+    public static void main(String[] args) {
+        // Main method
+        Main m = new Main();
+        m.start();
+    }
+    
+}
