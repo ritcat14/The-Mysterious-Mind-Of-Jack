817380220f9d3f619ca19e47965f99d6fa86fd69
ritca <ritca@DESKTOP-5A2JONP>
2017-12-08 15:55:52 +0000

    Start Menu Updates
    Changed start menu, moved buttons around. Exit button now works.

--- src/main/java/states/Start.java
+++ src/main/java/states/Start.java
@@ -20,9 +20,9 @@ public class Start extends State {
     
     public Start() {
         SoundHandler.loop("intro");
-        mainPanel = new GuiPanel(new Vector(200, 100), new Vector(StateHandler.WIDTH - 400, StateHandler.HEIGHT - 200), Tools.getImage("/gui/start.png"));
-        mainPanel.add(new GuiLabel(new Vector(250, 250), new Vector(150, 50), "MAIN MENU", Color.WHITE, new Font("Times New Java", Font.PLAIN, 50)));
-        mainPanel.add(new GuiButton(new Vector(250, 350), new Vector(90, 25), null, "START", new Font("Times New Java", Font.PLAIN, 25)) {
+        mainPanel = new GuiPanel(new Vector(100, 50), new Vector(StateHandler.WIDTH - 200, StateHandler.HEIGHT - 100), Tools.getImage("/gui/start.png"));
+        mainPanel.add(new GuiLabel(new Vector(150, 200), new Vector(150, 50), "MAIN MENU", Color.WHITE, new Font("Times New Java", Font.PLAIN, 50)));
+        mainPanel.add(new GuiButton(new Vector(230, 300), new Vector(90, 25), null, "START", new Font("Times New Java", Font.PLAIN, 25)) {
             @Override
             public boolean mousePressed(MousePressedEvent e) {
                 if (super.mousePressed(e)) {
@@ -34,7 +34,7 @@ public class Start extends State {
             
         }.setTextOffset(5, 23));
         
-        mainPanel.add(new GuiButton(new Vector(250, 450), new Vector(120, 25), null, "OPTIONS", new Font("Times New Java", Font.PLAIN, 25)) {
+        mainPanel.add(new GuiButton(new Vector(215, 350), new Vector(120, 25),  null, "OPTIONS", new Font("Times New Java", Font.PLAIN, 25)) {
             @Override
             public boolean mousePressed(MousePressedEvent e) {
                 if (super.mousePressed(e)) {
@@ -46,7 +46,7 @@ public class Start extends State {
             
         }.setTextOffset(5, 23));
         
-        GuiButton exit = new GuiButton(new Vector(250, 550), new Vector(80, 25), null, "EXIT", new Font("Times New Java", Font.PLAIN, 25)) {
+        GuiButton exit = new GuiButton(new Vector(240, 400), new Vector(65, 25),  null, "EXIT", new Font("Times New Java", Font.PLAIN, 25)) {
         	@Override
         	public boolean mousePressed(MousePressedEvent e) {
         		if (super.mousePressed(e)) {
@@ -56,7 +56,7 @@ public class Start extends State {
         		return false;
         	}
         	
-        };
+        }.setTextOffset(5, 23);;
         
         mainPanel.add(exit);
         
