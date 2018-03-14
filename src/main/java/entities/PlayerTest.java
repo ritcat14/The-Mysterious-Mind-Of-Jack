6f9d8a462dc4c41bafad47c8a2bb54a7c0bc77df
j-snowden98 <32417992+j-snowden98@users.noreply.github.com>
2018-03-14 22:08:16 +0000

    Test player movement
    Tried passing keyPressedEvents to player to check it moves accordingly (does nothing with invalid keys)

--- src/main/java/entities/PlayerTest.java
+++ src/main/java/entities/PlayerTest.java
@@ -4,8 +4,10 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import core.Main;
 import core.Map;
+import events.Event;
+import events.types.KeyPressedEvent;
+import events.types.KeyReleasedEvent;
 import handler.Vector;
 import states.Game;
 
@@ -13,20 +15,84 @@ public class PlayerTest {
 
 	@Test
 	public void testUpdate() {
-		Player p = new Player(new Map(new Game(1), 1), new Vector(500.0, 500.0), "/player/player.png", 0);
+		Player p = new Player(new Map(new Game(1), 1), new Vector(100.0, 300.0), "/player/player.png", 0);
 		Double startVel = p.velocity.x;
 		Double startX = p.pos.x;
-		System.out.println("Start x value = " + startX);
-		System.out.println("Start x velocity = " + startVel);
+		p.update();
 		
 		//try moving left
 		p.setLeft(true);
 		p.update();
-		System.out.println("Now x value = " + p.pos.x);
-		System.out.println("Now x velocity = " + p.velocity.x);
+		p.setLeft(false);
 		assertTrue(p.velocity.x < startVel);
 		assertTrue(p.pos.x < startX);
+		
+		//Left is now false - velocity should be 0 on this next update
+		p.update();
+		assertTrue(p.velocity.x == 0);
+		
+		//try moving right
+		startVel = p.velocity.x;
+		startX = p.pos.x;
+		p.setRight(true);
+		p.update();
+		p.setRight(false);
+		assertTrue(p.velocity.x > startVel);
+		assertTrue(p.pos.x > startX);
+		
+		//Move left again after moving right
+		startVel = p.velocity.x;
+		startX = p.pos.x;
+		p.setLeft(true);
+		p.update();
 		p.setLeft(false);
+		assertTrue(p.velocity.x < startVel);
+		assertTrue(p.pos.x < startX);
+		
+		//Do damage to player
+		double startHealth = p.getHealth();
+		p.doDamage(20.0);
+		p.update();
+		assertTrue(p.getHealth() < startHealth);
 	}
-
+	
+	@Test
+	public void testKeyPressed() {
+		Player p = new Player(new Map(new Game(1), 1), new Vector(500.0, 500.0), "/player/player.png", 0);
+		Double startX = p.pos.x;
+		Double startY = p.pos.y;
+		
+		//Valid - 'a' key (left)
+		p.keyPressed(new KeyPressedEvent(65, Event.Type.KEY_PRESSED));
+		p.update();
+		p.keyReleased(new KeyReleasedEvent(65, Event.Type.KEY_RELEASED));
+		assertTrue(p.pos.x < startX);
+		
+		//Valid - 'd' key (right)
+		startX = p.pos.x;
+		p.keyPressed(new KeyPressedEvent(68, Event.Type.KEY_PRESSED));
+		p.update();
+		p.keyReleased(new KeyReleasedEvent(68, Event.Type.KEY_RELEASED));
+		assertTrue(p.pos.x > startX);
+		
+		//Valid - 'w' key (jump/up)
+		p.keyPressed(new KeyPressedEvent(87, Event.Type.KEY_PRESSED));
+		p.update();
+		p.keyReleased(new KeyReleasedEvent(87, Event.Type.KEY_RELEASED));
+		assertTrue(p.pos.y < startY);
+		
+		//Valid - 'space' key (jump/up)
+		p.keyPressed(new KeyPressedEvent(32, Event.Type.KEY_PRESSED));
+		p.update();
+		p.keyReleased(new KeyReleasedEvent(32, Event.Type.KEY_RELEASED));
+		assertTrue(p.pos.y < startY);
+		
+		p.update();
+		//Invalid 'f' key
+		startX = p.pos.x;
+		p.keyPressed(new KeyPressedEvent(70, Event.Type.KEY_PRESSED));
+		p.update();
+		p.keyReleased(new KeyReleasedEvent(70, Event.Type.KEY_RELEASED));
+		assertTrue(p.pos.x == startX);
+	}
 }
